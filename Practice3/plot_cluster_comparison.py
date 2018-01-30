dff7f0bca31ffe09caa4b2c9749d80161fe74f75
m.mostafavi <m.mostafavi@M-MSTAFAVI>
2018-01-30 21:25:47 +0330

    تمرین سری سوم - کامنت گذاشتن بر روی کد plot_cluster_comparison

--- Practice3/plot_cluster_comparison.py
+++ Practice3/plot_cluster_comparison.py
@@ -22,106 +22,115 @@ While these examples give some intuition about the
 algorithms, this intuition might not apply to very high
 dimensional data.
 """
-print(__doc__)
 #متن اول صفحه را در خروجی چاپ میکند
+print(__doc__)
 
-import time
 #ماژول تایم را به کد اضافه میکند
+import time
 
-import warnings
 #ماژول خطا را به کد اضافه میکند
+import warnings
 
 
-import numpy as np
 #ماژول numpy را با نام مستعار np  به کد اضافه میکند
+import numpy as np
 
 
-import matplotlib.pyplot as plt
 #ماژول pyplot را به جهت رسم دیاگرام با نام مستعار plt  به کد اضافه میکند
+import matplotlib.pyplot as plt
 
 
-from sklearn import cluster, datasets, mixture
 #ماژول های مربوط به خوشه بندی، دیتاست ها و ترکیب گوسی را به کد اضافه میکند
+from sklearn import cluster, datasets, mixture
 
 
-from sklearn.neighbors import kneighbors_graph
 #ماژول گراف KNN را به کد اضفه میکند
+from sklearn.neighbors import kneighbors_graph
 
 
-from sklearn.preprocessing import StandardScaler
 #این ماژول برای نرمال سلزی داده ها استفاده می شود
+from sklearn.preprocessing import StandardScaler
 
 
-from itertools import cycle, islice
 #تکرر کننده های cycle , islice جهت ساختن ساختمان داده های تکرارکننده
+from itertools import cycle, islice
 
 
-np.random.seed(0)
 #initialiser  برای تابع رندوی numpy است
+np.random.seed(0)
+
 
 
 # ============
 # Generate datasets. We choose the size big enough to see the scalability
 # of the algorithms, but not too big to avoid too long running times
 # ============
-n_samples = 1500
 #تعداد نمونه هایی که در دیتاست های بعدی استفاده می شود را برابر 1500 نمونه در نظر میگیرد
+n_samples = 1500
 
 
-noisy_circles = datasets.make_circles(n_samples=n_samples, factor=.5,noise=.05)
 #یک دیتاست به شکل دایره دار درست می کند
+noisy_circles = datasets.make_circles(n_samples=n_samples, factor=.5,noise=.05)
 
-
-noisy_moons = datasets.make_moons(n_samples=n_samples, noise=.05)
 #دیتاست به شکل ماه درست میکند
+noisy_moons = datasets.make_moons(n_samples=n_samples, noise=.05)
 
 
-blobs = datasets.make_blobs(n_samples=n_samples, random_state=8)
 #دیتاست به شکل حباب حباب درست میکند
+blobs = datasets.make_blobs(n_samples=n_samples, random_state=8)
 
 
-no_structure = np.random.rand(n_samples, 2), None
 #دیتاست بدون اهیچ شکلی و به صورت رندوم درست میکند
+no_structure = np.random.rand(n_samples, 2), None
+
 
 # Anisotropicly distributed data
+#تعریف متغیر رندوم سایز
 random_state = 170
-X, y = datasets.make_blobs(n_samples=n_samples, random_state=random_state)
+
 #1500 نمونه  دوبعدی درست میکند
+X, y = datasets.make_blobs(n_samples=n_samples, random_state=random_state)
 
-
+#تعریف متغیر برای استفاده در ضرب برداری
 transformation = [[0.6, -0.6], [-0.4, 0.8]]
-X_aniso = np.dot(X, transformation)
-#برای آرایه های دو بعدی معادل ضرب برداری است
 
-aniso = (X_aniso, y)
+#برای آرایه های دو بعدی معادل ضرب برداری است
+X_aniso = np.dot(X, transformation)
+
 #تشکیل یک آرایه دو بعدی از X_aniso , y
+aniso = (X_aniso, y)
+
 
 
 # blobs with varied variances
+#ایجاد دیتاست حباب با واریانس های متفاوت
 varied = datasets.make_blobs(n_samples=n_samples,
                              cluster_std=[1.0, 2.5, 0.5],
                              random_state=random_state)
-#ایجاد دیتاست حباب با واریانس های متفاوت
+
 
 # ============
 # Set up cluster parameters
 # ============
-plt.figure(figsize=(9 * 2 + 3, 12.5))
 #یک شکل با انداز تعیین شده مشخص میکند
-plt.subplots_adjust(left=.02, right=.98, bottom=.001, top=.96, wspace=.05, hspace=.01)
+plt.figure(figsize=(9 * 2 + 3, 12.5))
+
 #طرح subplot را تنظیم میکند
+plt.subplots_adjust(left=.02, right=.98, bottom=.001, top=.96, wspace=.05, hspace=.01)
+
 
 plot_num = 1
 
+#ایجاد یک لیست برای پارامترهای دیفالت در دیتاست ها
 default_base = {'quantile': .3,
                 'eps': .3, #مقدار اپسیلون در الگوریتم های مبتنی بر چگالی
                 'damping': .9,
                 'preference': -200,
                 'n_neighbors': 10, #تعداد همسایه ها
                 'n_clusters': 3} #تعداد خوشه ها
-#ایجاد یک لیست برای پارامترهای دیفالت در دیتاست ها
 
 
+#ایجاد یک لیست از دیتاست های ایجاد شده به همرا  پارامترهای اولیه متفاوت
 datasets = [
     (noisy_circles, {'damping': .77, 'preference': -240,'quantile': .2, 'n_clusters': 2}),
     (noisy_moons, {'damping': .75, 'preference': -220, 'n_clusters': 2}),
@@ -129,9 +138,12 @@ datasets = [
     (aniso, {'eps': .15, 'n_neighbors': 2}),
     (blobs, {}),
     (no_structure, {})]
-#ایجاد یک لیست از دیتاست های ایجاد شده به همرا  پارامترهای اولیه متفاوت
+
 
 #در اینجا با ایجاد دو حلقه تو در تو انواع دیتاست ها را با الگوریتم های مختلف ترسیم میکند
+# متغییر اختصاص داده شده در هر دور
+# dataset  موجود در  هر مرحله از datasets را بر میگرداند
+# algo_params پارامترهای هر دیتاست را بر میگرداند
 for i_dataset, (dataset, algo_params) in enumerate(datasets):
     # متغییر اختصاص داده شده در هر دور
     #dataset  موجود در  هر مرحله از datasets را بر میگرداند
@@ -139,26 +151,29 @@ for i_dataset, (dataset, algo_params) in enumerate(datasets):
 
 
     # update parameters with dataset-specific values
-    params = default_base.copy()
     #یک کپی از پارامترهای دیفالت برای دیتاست ها را بر میگرداند
+    params = default_base.copy()
 
-    params.update(algo_params)
     #پارامترها با مقادیر پارامترهای هر مرحله آپدیت میکند
+    params.update(algo_params)
 
+    # X: نمونه های دوبعدی دیتا ست که در آرایه قرار دارد
+    # y: تارگت هر نمونه که البته در آرایه قراردارد
     X, y = dataset
-    #X: نمونه های دوبعدی دیتا ست که در آرایه قرار دارد
-   #y: تارگت هر نمونه که البته در آرایه قراردارد
 
+    # نرمال کردن مجموعه داده ها برای انتخاب پارامتر ساده تر
     X = StandardScaler().fit_transform(X)
-#نرمال کردن مجموعه داده ها برای انتخاب پارامتر ساده تر
+
 
     # estimate bandwidth for mean shift
+    # برآورد پهنای باند جهت استفاده از شیفت میانگین
     bandwidth = cluster.estimate_bandwidth(X, quantile=params['quantile'])
-#برآورد پهنای باند جهت استفاده از شیفت میانگین
+
 
     # connectivity matrix for structured Ward
-    connectivity = kneighbors_graph(X, n_neighbors=params['n_neighbors'], include_self=False)
     #ماتریس اتصال برای بخش ساختار یافته
+    connectivity = kneighbors_graph(X, n_neighbors=params['n_neighbors'], include_self=False)
+
 
     # make connectivity symmetric
     connectivity = 0.5 * (connectivity + connectivity.T)
@@ -166,36 +181,38 @@ for i_dataset, (dataset, algo_params) in enumerate(datasets):
     # ============
     # Create cluster objects
     # ============
+    # انتصاب الگوریتم شیفت میانگین که از یک کرنل فلت استفاده میکند به متغیر ms -
+    #  جهت عملکرد بهتر connectivity  قبلا محاسبه شده است که به جهت استفاده از کرنل RBF مهم است
     ms = cluster.MeanShift(bandwidth=bandwidth, bin_seeding=True)
-# انتصاب الگوریتم شیفت میانگین که از یک کرنل فلت استفاده میکند به متغیر ms -
-#  جهت عملکرد بهتر connectivity  قبلا محاسبه شده است که به جهت استفاده از کرنل RBF مهم است
 
+    # انتساب الگوریتمMiniBatchKMeans با متغیر two_means
     two_means = cluster.MiniBatchKMeans(n_clusters=params['n_clusters'])
-#انتساب الگوریتمMiniBatchKMeans با متغیر two_means
 
-    ward = cluster.AgglomerativeClustering( n_clusters=params['n_clusters'], linkage='ward', connectivity=connectivity)
-#یک نوع الگوریتم سلسله مراتبی هستش که اشیا خوشه بندی سلسله مراتبی را با استفاده از رویکرد پایین به بالا انجام می ده
-    # که متغیر linkage مجموع اختلاف مربع در تمام خوشه ها را کاهش می دهد
-
-    spectral = cluster.SpectralClustering(n_clusters=params['n_clusters'], eigen_solver='arpack',affinity="nearest_neighbors")
-# استفاده از الگوریتم SpectralClustering
-
-    dbscan = cluster.DBSCAN(eps=params['eps'])
-    # استفاده از الگوریتم DBSCAN
-
-    affinity_propagation = cluster.AffinityPropagation(damping=params['damping'], preference=params['preference'])
-# الگوریتم AffinityPropagation خوشه ها را با ارسال پیام ها بین جفت نمونه تا همگرایی ایجاد می کند
-
-    average_linkage = cluster.AgglomerativeClustering( linkage="average", affinity="cityblock",n_clusters=params['n_clusters'], connectivity=connectivity)
     # یک نوع الگوریتم سلسله مراتبی هستش که اشیا خوشه بندی سلسله مراتبی را با استفاده از رویکرد پایین به بالا انجام می ده
     # که متغیر linkage مجموع اختلاف مربع در تمام خوشه ها را کاهش می دهد
-    #در اینجا برای فاصله از فاصله منهتن استفاده کرده است
+    ward = cluster.AgglomerativeClustering( n_clusters=params['n_clusters'], linkage='ward', connectivity=connectivity)
+
+    # استفاده از الگوریتم SpectralClustering
+    spectral = cluster.SpectralClustering(n_clusters=params['n_clusters'], eigen_solver='arpack',affinity="nearest_neighbors")
+
+    # استفاده از الگوریتم DBSCAN
+    dbscan = cluster.DBSCAN(eps=params['eps'])
+
+    # الگوریتم AffinityPropagation خوشه ها را با ارسال پیام ها بین جفت نمونه تا همگرایی ایجاد می کند
+    affinity_propagation = cluster.AffinityPropagation(damping=params['damping'], preference=params['preference'])
+
+    # یک نوع الگوریتم سلسله مراتبی هستش که اشیا خوشه بندی سلسله مراتبی را با استفاده از رویکرد پایین به بالا انجام می ده
+    # که متغیر linkage مجموع اختلاف مربع در تمام خوشه ها را کاهش می دهد
+    # در اینجا برای فاصله از فاصله منهتن استفاده کرده است
+    average_linkage = cluster.AgglomerativeClustering( linkage="average", affinity="cityblock",n_clusters=params['n_clusters'], connectivity=connectivity)
+
+    # الگوریتم Birch یک درخت به نام خصوصیت ویژگی درخت (CFT) برای داده های داده شده ایجاد می کند
     birch = cluster.Birch(n_clusters=params['n_clusters'])
-#الگوریتم Birch یک درخت به نام خصوصیت ویژگی درخت (CFT) برای داده های داده شده ایجاد می کند
 
+    # استفاده از الگوریتم ماتریس گوسی
     gmm = mixture.GaussianMixture(n_components=params['n_clusters'], covariance_type='full')
-#استفاده از الگوریتم ماتریس گوسی
 
+    # ایجاد متغیر آرایه با استفاده از متغیرهای الگوریتم های ایجاد شده
     clustering_algorithms = (
         ('MiniBatchKMeans', two_means),
         ('AffinityPropagation', affinity_propagation),
@@ -207,12 +224,16 @@ for i_dataset, (dataset, algo_params) in enumerate(datasets):
         ('Birch', birch),
         ('GaussianMixture', gmm)
     )
-# ایجاد متغیر
 
+
+#در اینجا یک حلقه ایجاد شده  که الگوریتم های ایجاد شده در بلارا برو داده های گرفته شده ارز حلقه بالا اجرا میکند
     for name, algorithm in clustering_algorithms:
+
+        #شروع زمان اجرای الگوریتم
         t0 = time.time()
 
         # catch warnings related to kneighbors_graph
+        #در اینجا به جهت جلوگیری از خطا هایی که ممکن است در الگوریتم kneighbors_graph رخ دهد خطاها چک میشود
         with warnings.catch_warnings():
             warnings.filterwarnings(
                 "ignore",
@@ -225,31 +246,40 @@ for i_dataset, (dataset, algo_params) in enumerate(datasets):
                 message="Graph is not fully connected, spectral embedding" +
                 " may not work as expected.",
                 category=UserWarning)
+            #اگر خطا رخ ندهد الگوریتم را بر روی دیتاست اعمال میکند
             algorithm.fit(X)
-
+        #زمان تمام شدن الگوریتم
         t1 = time.time()
+        #چک میکند که labels_  در خروجی الگوریتم وجود دارد یا خیر اگر لیبلی در خورد دیتا وجود داشته باشد در اینجا متوجه میشود
+        #کلن خوشه بندی های به دونوع تقسیم میشود : یکی اینکه لیبل های خوشه بندی جزو یکی از ویژگی ها است
+        # و دیگر اینکه با فیت کردن لیل های خروجی برای هر خوشه مشخص شود
         if hasattr(algorithm, 'labels_'):
+            #اگر لیبل خوشه جزو یکی از ویژگی ها باشد در اینجا در y_pred قرار میگیرد
             y_pred = algorithm.labels_.astype(np.int)
         else:
+            #پیشبینی خوشه که در هر نمونه در کدام دسته قرار میگیرد
             y_pred = algorithm.predict(X)
-
+        #سابپلات های مختلف برای هر خوش در هر الگوریتم درست میکند
         plt.subplot(len(datasets), len(clustering_algorithms), plot_num)
-        if i_dataset == 0:
-            plt.title(name, size=18)
 
+        if i_dataset == 0:
+            plt.title(name, size=10)
+        #رنگهای مختلف برای استفاده در پلات
         colors = np.array(list(islice(cycle(['#377eb8', '#ff7f00', '#4daf4a',
                                              '#f781bf', '#a65628', '#984ea3',
                                              '#999999', '#e41a1c', '#dede00']),
                                       int(max(y_pred) + 1))))
+        #یک اسکاتر درست میکند برای قرارداد شکل های ایجاد شده در پلات
         plt.scatter(X[:, 0], X[:, 1], s=10, color=colors[y_pred])
 
+        #تنظیمات مربوط به نمایش پلات
         plt.xlim(-2.5, 2.5)
         plt.ylim(-2.5, 2.5)
         plt.xticks(())
         plt.yticks(())
-        plt.text(.99, .01, ('%.2fs' % (t1 - t0)).lstrip('0'),
-                 transform=plt.gca().transAxes, size=15,
-                 horizontalalignment='right')
+        #مقدار زمان اجرای الگوریتم را در هر شکل قرار میدهد
+        plt.text(.99, .01, ('%.2fs' % (t1 - t0)).lstrip('0'), transform=plt.gca().transAxes, size=15,horizontalalignment='right')
+        #یک واحد به شماره پلات برای استفاده در دور بعدی استفاده میکند
         plot_num += 1
-
+#نمایش پلات و نمودارها
 plt.show()
